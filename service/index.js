cf219ac27867450d795c00b9de7c7994369a0e4f
kinggreyson <kingg14@byu.edu>
2026-03-24 22:40:21 -0600

    Made database conversions in index

--- service/index.js
+++ service/index.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
-// In-memory storage (for now)
-const users = [];
-const tierLists = [];
+
+const db = require('./database.js');
+db.connect();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -20,27 +20,28 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //Registering
 app.post('/api/auth/register', async (req, res) => {
-    const { username, password } = req.body;
-    if (users.find(u => u.username == username)) { //IF USERNAME IS ALREADY TAKEN
-        return res.status(409).json({ msg: 'Username already taken'});
-    }
-    const hashed = await bcrypt.hash(password, 10); //Password encrypting
-    const token = uuid.v4();
-    users.push({ username, password: hashed, token });
-    res.cookie('token', token, { sameSite: 'strict', httpOnly: true });
-    res.json({ username }); 
+  const { username, password } = req.body;
+  if (await db.getUser(username)) {
+    return res.status(409).json({ msg: 'Username already taken' });
+  }
+  const hashed = await bcrypt.hash(password, 10);
+  const token = uuid.v4();
+  const user = await db.createUser(username, hashed, token);
+  res.cookie('token', token, { sameSite: 'strict', httpOnly: true });
+  res.json({ username });
 });
 
 // Login
-app.post('/api/auth/login', async (req,res) => {
-    const {username, password } = req.body;
-    const user = users.find(u => u.username === username);
-    if (!user || !(await bcrypt.compare(password, user.password))) { //Incorrect Login
-        return res.status(401).json({msg: 'Invalid Credentials'});
-    }
-    user.token = uuid.v4();
-    res.cookie('token', user.token, { sameSite: 'strict', httpOnly: true});
-    res.json({ username });
+app.post('/api/auth/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await db.getUser(username);
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ msg: 'Invalid Credentials' });
+  }
+  const token = uuid.v4();
+  await db.updateUserToken(username, token);
+  res.cookie('token', token, { sameSite: 'strict', httpOnly: true });
+  res.json({ username });
 });
 
 // Logout
@@ -53,13 +54,12 @@ app.delete('/api/auth/logout', (req, res) => {
 });
 
 //Security
-function requireAuth(req, res, next)
-{
-    const token = req.cookies.token;
-    const user = users.find(u => u.token === token);
-    if (!user) return res.status(401).json({ msg: 'Unauthorized' });
-    req.user = user;
-    next();
+async function requireAuth(req, res, next) {
+  const token = req.cookies.token;
+  const user = await db.getUserByToken(token);
+  if (!user) return res.status(401).json({ msg: 'Unauthorized' });
+  req.user = user;
+  next();
 }
 
 //Save Tier List
