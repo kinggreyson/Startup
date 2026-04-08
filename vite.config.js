33a2adbcf97e1ead28f0bc0f9f8ea45fe57c3cc6
kinggreyson <kingg14@byu.edu>
2026-04-07 18:55:20 -0600

    Updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': 'http://localhost:4000',
-        },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000',
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
     },
+  },
 });
